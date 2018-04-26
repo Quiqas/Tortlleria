a5a32d8e1fc0a9ef685e7425f4f2e558101a68d9
Cristhian <albert502@outlook.com>
2018-04-25 20:17:41 -0600

    Hola

---
+++ ventas.php
@@ -0,0 +1,72 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>A&C Tortillas</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="jumbotron.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+      <a class="navbar-brand" href="index.html"><img src="images/Logo.png" class="responsive" style="width: 160px; height: 40px"></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      
+    </nav>
+
+    <main role="main">
+
+      <!-- Main jumbotron for a primary marketing message or call to action -->
+      <div class="jumbotron">
+        <div class="container">
+        	<br>
+          <h1 class="display-3">Ventas Totales</h1>
+          <div class="row">
+            <div class="card" style="width: 18rem;">
+              <img class="card-img-top" src="images/Male-símbolo2.svg" alt="Card image cap">
+              <div class="card-body">
+                <p class="card-text">Se vendieron un total de <?php echo rand(1,150);?> kilos de tortillas a  clientes masculinos.</p>
+              </div>
+            </div>
+
+
+            <div class="card" style="width: 18rem;">
+              <img class="card-img-top" src="images/Símbolo_femenino.png" alt="Card image cap">
+              <div class="card-body">
+                <p class="card-text">Se vendieron un total de <?php echo rand(1,150);?> kilos de tortillas a  clientes femeninos.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      
+        <hr>
+
+      </div> <!-- /container -->
+
+    </main>
+
+    <footer class="container">
+      <p>&copy; A&C Tortillas 2017-2018</p>
+    </footer>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="css/bootstrap/bootstrap.min.js"></script>
+  </body>
+</html>
